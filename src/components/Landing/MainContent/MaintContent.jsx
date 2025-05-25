b41eb0a25d1c67f3578d4aad239cbffaa509bbaa
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2025-05-25 21:29:36 +0330

    crste component section12

--- src/components/Landing/MainContent/MaintContent.jsx
+++ src/components/Landing/MainContent/MaintContent.jsx
@@ -2,6 +2,7 @@ import Border from "../../../ui/border";
 import Button from "../../../ui/Button";
 import Footer from "../Footer";
 import Section1 from "./Section1";
+import Section12 from "./Section12";
 import Section2 from "./Section2";
 import Section3 from "./Section3";
 import Section4 from "./Section4";
@@ -10,7 +11,8 @@ import Section55 from "./Section55";
 function MaintContent() {
   return (
     <div className="mx-auto">
-      <Section1 />
+      {/* <Section1 /> */}
+      <Section12 />
       <Border />
       <div className="mt-[80px]">
         <Button>حالت نمایش</Button>
--- src/components/Landing/MainContent/Section12.jsx
+++ src/components/Landing/MainContent/Section12.jsx
@@ -1,3 +1,4 @@
+import ButtonBig from "../../../ui/ButtonBig";
 import H1 from "../../../ui/H1";
 
 function Section1() {
@@ -7,16 +8,15 @@ function Section1() {
         سامانه مدیریت <span className="text-[#0182FE]">هوشمند</span>
         <br /> سفارشهای اینترنتی
       </H1>
-
+      <ButtonBig color="#0182FE" picture="/src/assets/image/video-circle.png">
+        مشاهده ویديومعرفی
+      </ButtonBig>
       <p className="mt-10 text-center text-[16px] leading-[180%] font-normal text-[#171F26]">
         کارت ساز آمده تا دیگر نیازی به یادداشت سفارش های خود در دفتر و یا سررسید
         نباشید.به راحتی سفارشات خود را <br />
         مدیریت کنید و آن ها را تعیین وضعیت کنید.حتی به راحتی برگه ارسال پرینت
         کنید و آن را اختصاصی استفاده کنید.
       </p>
-      <div className="mx-auto mt-6 h-6 w-6">
-        <img src="/src/assets/image/arrow-down.png" />
-      </div>
     </div>
   );
 }
