746dc43092e2c2193794f1cd68bea27e17609fd3
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2025-05-25 18:58:45 +0330

    fixed:change menu

--- src/components/Landing/Header/Menu.jsx
+++ src/components/Landing/Header/Menu.jsx
@@ -1,13 +1,15 @@
 function Menu() {
   return (
-    <div>
+    <div className="flex gap-8">
       {/* pl-[333px] */}
-      <ul className="flex list-none gap-8 pl-32 text-xl">
+      <ul className="flex list-none gap-8 text-xl">
         <li>آپدیت ها</li>
         <li> سوالات متداول</li>
         <li> تماس با ما </li>
-        <li className="text-[#0182FE]">ورود یا ثبت نام</li>
       </ul>
+      <button className="relative bottom-4 rounded-2xl border-1 border-[#0182FE] bg-white px-8 py-4 text-[#0182FE]">
+        ورود یا ثبت نام
+      </button>
     </div>
   );
 }
