39e6b1e46e1c339e2de4a7c3f9654b94d0a678f7
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2025-05-28 12:57:59 +0330

    fixed:compelete ContactUs component

--- src/components/Landing/ContactUs/ContactUs.jsx
+++ src/components/Landing/ContactUs/ContactUs.jsx
@@ -4,39 +4,59 @@ import Section4 from "../MainContentP2Mobile/Section4";
 import Section55 from "../MainContentP2Mobile/Section55";
 function ContactUs() {
   return (
-    <div>
-      <div className="mt-[112px] flex justify-start">
-        <p className="text-[32px] leading-[180%] font-[700px] text-[#0182FE]">
+    <div className="relative">
+      <div className="relative mt-[112px] flex justify-start">
+        <p className="pl-4 text-[32px] leading-[180%] font-[700px] text-[#0182FE]">
           تماس با ما
         </p>
-        <div className="mt-[60px] h-[544px] w-[808px] py-16">
-          <div className="text-[16px] leading-[180%] font-normal text-[#171F26]">
-            <p>
+        <div className="absolute top-[30px] right-0 mt-[60px] ml-40 flex flex-col">
+          <div className="w-full text-[16px] leading-[180%] font-normal text-[#171F26] capitalize">
+            <h3>
               جهت تماس با ما می توانید با یکی از مسیرهای زیر با ما ارتباط برقرار
               :کنید
+            </h3>
+          </div>
+          <div className="mt-[20px] flex h-[64px] w-[392px] gap-4 rounded-[8px] border-1 border-[#D12879] py-[19px] text-[#D12879]">
+            <div className="mr-[118px] flex gap-2">
+              <img
+                src="/src/assets/image/instagram.png"
+                className="h-[24px] w-[24px]"
+              />
+              <p>پیام در دایرکت اینستاگرام</p>
+            </div>
+          </div>
+          <div className="mt-[20px] flex h-[64px] w-[392px] rounded-[8px] border-1 border-[#2FDB9D] py-[19px] text-[#2FDB9D]">
+            <div className="mr-[118px] flex gap-2">
+              <img
+                src="/src/assets/image/whatsapp.png"
+                className="h-[24px] w-[24px]"
+              />
+              <p> تماس در واتساپ</p>
+            </div>
+          </div>
+
+          <div className="mt-[26px] flex w-full flex-col items-start">
+            <p className="text-[16px] leading-[180%] font-normal text-[#171F26] capitalize">
+              همچنین از ایمیل زیر میتوانید استفاده کنید:
             </p>
           </div>
-          <div className="h-[64px] w-[392px] rounded-[8px] border-1 border-[#D12879] text-[#D12879]">
-            پیام در دایرکت اینستاگرام
-          </div>
-          <div className="h-[64px] w-[392px] rounded-[8px] border-1 border-[#2FDB9D] text-[#2FDB9D]">
-            تماس در واتساپ
-          </div>
-          <div className="left-[370.84px] mx-auto mt-[334.92] h-[175.23px] w-[175.23px] rounded-[143.37px] border-[1.99px] border-[#0182FE] bg-gradient-to-b from-[#0182FE] to-[##DFEFFF]">
-            <img
-              src="/src/assets/image/message.png"
-              className="m-[39.83px] h-[95.58px] w-[95.58px]"
-            />
-          </div>
-          <div className="text-[16px] leading-[180%] font-normal text-[#171F26]">
-            <p>همچنین از ایمیل زیر میتوانید استفاده کنید:</p>
-          </div>
-          <div className="text-[20px] leading-[180%] font-normal text-[#0182FE]">
-            <Link to="Cartsaz.Com">Cartsaz.Com@Gmail.Com</Link>
+
+          <div className="py-2 text-[20px] leading-[180%] font-normal text-[#0182FE] underline">
+            <Link to="Cartsaz.Com" className="flex items-start">
+              Cartsaz.Com@Gmail.Com
+            </Link>
           </div>
         </div>
       </div>
-      <div className="mt-[200px]">
+      <div className="absolute top-[180px] left-[300.84px] flex h-[211.08px] w-[211.08px] items-center justify-center">
+        <div className="flex h-[175.23px] w-[175.23px] items-center justify-center rounded-[143.37px] border-[1.99px] border-[#0182FE] bg-gradient-to-b from-[#0182FE] to-[#DFEFFF]">
+          <img
+            src="/src/assets/image/message.png"
+            className="h-[95.58px] w-[95.58px]"
+          />
+        </div>
+      </div>
+      <div className="mt-[430px]">
         <Section4 />
       </div>
       <div className="mt-[60px]">
