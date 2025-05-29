5bbe022487e758ae7d075b92e571582d3ae45eaa
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2025-05-29 22:44:51 +0330

    fixed:create menu component Page5

---
+++ src/components/Landing/Page5/Page5.jsx
@@ -0,0 +1,21 @@
+function Page5() {
+  return (
+    <div className="w-screen border-t border-b border-[#CBCBCB]">
+      <div className="mx-auto flex h-[80px] w-full max-w-screen-xl items-center gap-4 px-4">
+        <div className="h-6 w-6">
+          <img src="src/assets/image/arrow-left.png" alt="Arrow" />
+        </div>
+        <div className="h-[40px] w-[40px] rounded-[8px] border border-[#CBCBCB]">
+          <img src="src/assets/image/Frame 32.png" alt="logo" />
+        </div>
+        <div>
+          <h6 className="flex items-center text-[16px] leading-[140%] font-bold text-black capitalize">
+            فروشگاه کارت ساز
+          </h6>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Page5;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,8 +1,9 @@
 // import Footer from "../components/Landing/Footer";
 // import ContactUs from "../components/Landing/ContactUs/ContactUs";
 import FooterMobile from "../components/Landing/FooterMobile";
+import Page5 from "../components/Landing/Page5/Page5";
 // import Page3 from "../components/Landing/Page3.jsx/Page3";
-import Page7 from "../components/Landing/Page7/Page7";
+// import Page7 from "../components/Landing/Page7/Page7";
 // import FrequentlyQuestions from "../components/Landing/FrequentlyQuestions/FrequentlyQuestions";
 // import Header from "../components/Landing/Header/Header";
 // import Page6 from "../components/Landing/Page6.jsx/Page6";
@@ -25,7 +26,8 @@ function Landing() {
 
       {/* <Page6 /> */}
       {/* <Page3 /> */}
-      <Page7 />
+      {/* <Page7 /> */}
+      <Page5 />
     </div>
   );
 }
