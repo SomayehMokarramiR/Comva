16b55350993aa64bfc9653f34d70f8cf0c8f5c53
SomayehMokarrami <146121921+SomayehMokarrami@users.noreply.github.com>
2025-05-31 20:29:27 +0330

    change landing

--- src/components/Landing/FrequentlyQuestions/Question.jsx
+++ src/components/Landing/FrequentlyQuestions/Question.jsx
@@ -3,10 +3,13 @@ function Question({ questionText, id }) {
     <div className="h-full w-full">
       <select className="border-#CBCBCB mt-4 h-[64px] w-full rounded-[8px] border-1 p-4">
         <option key={id}>
-          {/* <ul>
-            <li className="marker:text-#0182FE list-disc">{questionText}</li>
-          </ul> */}
-          {questionText}
+          <label
+            htmlFor={`question-${id}`}
+            className="text-[16px] text-[#74787C]"
+          >
+            <span className="h-3 w-3 rounded-full bg-[#0182FE]"></span>
+            {questionText}
+          </label>
         </option>
       </select>
     </div>
@@ -14,3 +17,23 @@ function Question({ questionText, id }) {
 }
 
 export default Question;
+// function Question({ questionText, id }) {
+//   return (
+//     <div className="flex flex-col space-y-2">
+//       <div className="flex items-center space-x-2"></div>
+
+//       <select
+//         id={`question-${id}`}
+//         className="h-[64px] w-full rounded-[8px] border border-[#CBCBCB] p-4"
+//       >
+//         <option>
+//           <p className="text-[16px] text-[#74787C]">
+//             {` ${(<span className="h-3 w-3 rounded-full bg-[#0182FE]"></span>)} ${questionText} `}
+//           </p>
+//         </option>
+//       </select>
+//     </div>
+//   );
+// }
+
+// export default Question;
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -8,20 +8,20 @@ import FooterMobile from "../components/Landing/FooterMobile";
 // import Page7 from "../components/Landing/Page7/Page7";
 // import Page6 from "../components/Landing/Page6.jsx/Page6";
 
-import FrequentlyQuestions from "../components/Landing/FrequentlyQuestions/FrequentlyQuestions";
+// import FrequentlyQuestions from "../components/Landing/FrequentlyQuestions/FrequentlyQuestions";
 // import ContactUs from "../components/Landing/ContactUs/ContactUs";
 
 // import MaintContent from "../components/Landing/MainContent/MaintContent";
 // import MaintContent2 from "../components/Landing/MainContentP2/MaintContent2";
-// import MaintContent3 from "../components/Landing/MainContentP2Mobile/MaintContent3";
+import MaintContent3 from "../components/Landing/MainContentP2Mobile/MaintContent3";
 
 function Landing() {
   return (
     <div>
       <Header />
       {/* <MaintContent2 /> */}
-      {/* <MaintContent3 /> */}
-      <FrequentlyQuestions />
+      <MaintContent3 />
+      {/* <FrequentlyQuestions /> */}
       {/* <ContactUs /> */}
 
       <FooterMobile />
